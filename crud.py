a7fe2a862dbfdb43e008c04c07444e7567b15dca
Bohdan11Dii <bodyabrovdiy@gmail.com>
2025-08-31 21:01:56 +0200

    Create get and post method for blog types

--- crud.py
+++ crud.py
@@ -1,13 +1,18 @@
 from sqlalchemy.orm import Session
 
-from db.models import DBBlogType
+from db import models
 from schemas import BlogTypeCreate
 
 def get_all_blog_types(db: Session):
-    return db.query(DBBlogType).all()
+    return db.query(models.DBBlogType).all()
+
+def get_blog_type_by_name(db: Session, name: str):
+    return (
+        db.query(models.DBBlogType).filter(models.DBBlogType.name == name).first()
+    )
 
 def create_blog_type(db: Session, blog_type: BlogTypeCreate):
-    db_blog_type = DBBlogType(
+    db_blog_type = models.DBBlogType(
         name=blog_type.name,
     )
     db.add(db_blog_type)
--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from db.engine import SessionLocal
 
 app = FastAPI()
 
 
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
-def root():
+def root() -> dict:
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/blog_types/", response_model=list[schemas.BlogType])
+def read_blog_types(db: Session = Depends(get_db)):
+    return crud.get_all_blog_types(db=db)
+
+
+@app.post("/blog_types/", response_model=schemas.BlogType)
+def create_blog_types(
+        blog_type: schemas.BlogTypeCreate,
+        db: Session = Depends(get_db)
+):
+    db_blog_type = crud.get_blog_type_by_name(db=db, name=blog_type.name)
+    if db_blog_type:
+        raise HTTPException(
+            status_code=400,
+            detail="Blog type already exists"
+        )
+
+    return crud.create_blog_type(db=db, blog_type=blog_type)
